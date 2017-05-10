db0afb0907a7b35da0eb62939b67d2c3e6d1c3be
kasper95 <kove_kasper@outlook.com>
2017-05-10 18:15:51 -0400

    +split on comma and add to 'map?'

--- src/filtering_purchases_clj/core.clj
+++ src/filtering_purchases_clj/core.clj
@@ -5,5 +5,11 @@
 (defn -main []
   (let [purchases (slurp "purchases.csv")
         purchases (str/split-lines purchases)
+
+        ; goes over each line and split by commas.
+        ; makes the lines be vectors containing a separate string for each column
+        purchases (map (fn [line]
+                         (str/split line #","))
+                       purchases)
         ]
               purchases))
